4203082834b8b6d01a6442ddbbc0ea96fd6141e5
Emgmbakri <mrm507415@gmail.com>
2024-02-27 23:41:12 +0400

    new node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,6 +16,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->parent = prent;
 	new_node->left = NULL;
-	new_node->right = Null;
+	new_node->right = NULL;
 	return (new_node);
 }
